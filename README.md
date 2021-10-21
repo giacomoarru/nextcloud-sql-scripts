946e2e59588821a2159d206d4074ce25a4266994
giacomoarru <giacomo.arru@gmail.com>
2021-10-21 09:03:33 +0100

    List of files per user

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # nextcloud-sql-scripts
+
+This repository contains useful SQL queries that help with Nextcloud management.
+
+
+
+* list_of_files_per_user.sql
+This query will return the list of files owned by a user.
+It works when S3 Object Storage is used as primary storage.
+
---
+++ list_of_files_per_user.sql
@@ -0,0 +1,22 @@
+SET @username = 'username'; # set username here
+SELECT 
+    u.uid,
+    f.fileid,
+    f.path,
+    f.name,
+    f.size,
+    FROM_UNIXTIME(f.mtime)
+FROM
+    nextcloud.oc_filecache f,
+    nextcloud.oc_storages s,
+    nextcloud.oc_users u
+WHERE
+    u.uid = @username
+        AND s.id = CONCAT('object::user:', u.uid)
+        AND f.storage = s.numeric_id
+        AND s.available = 1
+        AND f.mimetype != 2
+        AND path NOT IN ('cache' , 'files', '')
+        AND path NOT LIKE 'files_trashbin%'
+        AND size > 0
+ORDER BY path ASC
